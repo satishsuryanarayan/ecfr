187d55bf1eddaebf8bccf497d00bc637ae52452b
satishsuryanarayan <satsur@hotmail.com>
2025-06-06 15:26:39 -0500

    Matched dtos to model

--- api/dtos/agency.py
+++ api/dtos/agency.py
@@ -2,17 +2,19 @@ from marshmallow import Schema, fields, post_load
 
 
 class Agency:
-    def __init__(self, id: str, name: str) -> None:
+    def __init__(self, id: str, name: str, parent_id: str) -> None:
         self.id = id
         self.name = name
+        self.parent_id = parent_id
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name}, parent_id={self.parent_id})"
 
 
 class AgencySchema(Schema):
     id = fields.String(required=True)
     name = fields.String(required=True)
+    parent_id = fields.String(required=False, allow_none=True)
 
     @post_load
     def make_agency(self, data: dict, **kwargs: dict) -> Agency:
--- api/dtos/cfr_insight.py
+++ api/dtos/cfr_insight.py
@@ -3,25 +3,21 @@ from marshmallow import Schema, fields, post_load
 
 
 class CFRInsight:
-    def __init__(self, cfr_reference_id: int, agency_id: str, parent_agency_id: str, date: datetime, word_count: str, restricted_word_count: str) -> None:
+    def __init__(self, cfr_reference_id: int, date: datetime, word_count: str, restrictive_terms_count: str) -> None:
         self.cfr_reference_id = cfr_reference_id
-        self.parent_agency_id = parent_agency_id
-        self.agency_id = agency_id
         self.date = date
         self.word_count = word_count
-        self.restricted_word_count = restricted_word_count
+        self.restrictive_terms_count = restrictive_terms_count
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(cfr_reference_id={self.cfr_reference_id}, agency_id={self.agency_id}, parent_agency_id={self.parent_agency_id}, date={self.date}, word_count={self.word_count}, restricted_word_count={self.restricted_word_count})"
+        return f"{self.__class__.__name__}(cfr_reference_id={self.cfr_reference_id}, date={self.date}, word_count={self.word_count}, restricted_word_count={self.restrictive_terms_count})"
 
 
 class CFRInsightSchema(Schema):
     cfr_reference_id = fields.Integer(required=True)
-    agency_id = fields.String(required=True)
-    parent_agency_id = fields.String(required=False, allow_none=True)
     date = fields.Date(required=True)
     word_count = fields.Integer(required=True)
-    restricted_word_count = fields.Integer(required=True)
+    restrictive_terms_count = fields.Integer(required=True)
 
     @post_load
     def make_cfr_insight(self, data: dict, **kwargs: dict) -> CFRInsight:
