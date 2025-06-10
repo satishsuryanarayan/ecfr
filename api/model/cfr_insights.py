ee979e46c372ecaae8526f7e3427a1161ba37497
satishsuryanarayan <satsur@hotmail.com>
2025-06-10 07:49:05 -0500

    Partition cfr_insights table by agency_id. Since InnoDB does not support partition by ForeignKey, remove the ForeignKey specification from agancy_id and parent_agency_id

--- api/model/cfr_insights.py
+++ api/model/cfr_insights.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, Date, String
+from sqlalchemy import Table, Column, Integer, Date, String, event, DDL
 from sqlalchemy.sql.schema import ForeignKey
 
 from api.model.database import metadata
@@ -7,10 +7,18 @@ CFR_Insights = Table(
     "cfr_insights",
     metadata,
     Column("cfr_reference_id", Integer, ForeignKey("cfr_references.id"), nullable=False, index=True),
-    Column("agency_id", Integer, ForeignKey("agencies.id"), nullable=False, index=True),
-    Column("parent_agency_id", Integer, ForeignKey("agencies.id"), nullable=True, index=True),
+    Column("agency_id", Integer, nullable=False, index=True),
+    Column("parent_agency_id", Integer, nullable=True, index=True),
     Column("date", Date, nullable=False, index=True),
     Column("word_count", Integer, nullable=False),
     Column("checksum", String(256), nullable=False),
     Column("restrictive_terms_count", Integer, nullable=False),
+)
+
+event.listen(
+    metadata,
+    "after_create",
+    DDL(
+        "ALTER TABLE cfr_insights PARTITION BY HASH(agency_id) PARTITIONS 350"
+    ),
 )
